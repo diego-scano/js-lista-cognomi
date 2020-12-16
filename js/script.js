c93123806bfeb7125d15ae2b66c49739aa71e183
Diego <diego.scano77@gmail.com>
2020-12-16 15:06:37 +0100

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-var listaCognomi = document.getElementById('lista-cognomi');
-var user = prompt('Inserisci il tuo cognome!');
-listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var output = document.getElementById('output');
+var user = prompt('Inserisci il tuo cognome (con la prima lettera maiuscola)!');
+var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 listaCognomi.push(user);
 listaCognomi.sort();
 
 for (var i = 0; i < listaCognomi.length; i++) {
-  listaCognomi.innerHTML += '<li>' + listaCognomi[i] + '</li>';
+  output.innerHTML += '<li>' + listaCognomi[i] + '</li>';
 }
+
+var index = listaCognomi.indexOf(user);
+output.innerHTML += 'La tua posizione è: ' + (index + 1);
